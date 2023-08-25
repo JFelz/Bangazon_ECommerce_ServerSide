fcf072bb03a97be1f8be597212b2616430bbc084
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-08-25 17:17:10 -0500

    debugging

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,17 +1,29 @@
-﻿namespace Bangazon_ECommerce_ServerSide.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bangazon_ECommerce_ServerSide.Models
 {
     public class Order
     {
         public int Id { get; set; }
+        [Required]
         public int CustomerId { get; set; }
+        [Required]
         public Customer customer { get; set; }
+        [Required]
         public int SellerId { get; set; }
+        [Required]
         public Seller seller { get; set; }
+        [Required]
         public int ProductId { get; set; }
+        [Required]
         public Product product { get; set; }
+        [Required]
         public DateTime DatePurchased { get; set; }
+        [Required]
         public int OrderStatusId { get; set; }
+        [Required]
         public OrderStatus status { get; set; }
+        [Required]
         public DateTime StatusUpdateDate { get; set; }
     }
 }
--- Models/Seller.cs
+++ Models/Seller.cs
@@ -1,12 +1,18 @@
-﻿namespace Bangazon_ECommerce_ServerSide.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bangazon_ECommerce_ServerSide.Models
 {
     public class Seller
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public long PhoneNumber { get; set; }
         public string imageUrl { get; set; }
+        [Required]
         public int PaymentType_Id { get; set; }
     }
 }
