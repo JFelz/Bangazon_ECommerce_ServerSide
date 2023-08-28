01621022775d77b1a6d049647cc2323a096a5073
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-08-28 16:32:53 -0500

    finished JOIN tables

---
+++ Models/SellerOrder.cs
@@ -0,0 +1,8 @@
+﻿namespace Bangazon_ECommerce_ServerSide.Models
+{
+    public class SellerOrder
+    {
+        public int OrderId { get; set; }
+        public int SellerId { get; set; }
+    }
+}
