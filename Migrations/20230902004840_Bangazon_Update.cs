f629b8914dd982a127327fff008596279338af11
Jovanni Feliz <kfeliz@c.ringling.edu>
2023-09-02 13:28:36 -0500

    Cors added

---
+++ Migrations/20230902004840_Bangazon_Update.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Bangazon_ECommerce_ServerSide.Migrations
+{
+    public partial class Bangazon_Update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    uid = table.Column<string>(type: "text", nullable: false),
+                    displayName = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    photoURL = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "displayName", "email", "photoURL", "uid" },
+                values: new object[] { 1, "Mark Hallow", "mhallow998@gmail.com", "https://st4allthings4p4ci.blob.core.windows.net/allthingshair/allthingshair/wp-content/uploads/sites/3/2017/09/professional-mens-hairstyles-combed-back-min.jpg", "Y5m9isPKZ9Ugs8ptRg9w1rVe6xs1" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bangazon_ECommerce_ServerSide.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        [Required]
+        public string uid { get; set; }
+        public string displayName { get; set; }
+        public string email { get; set; }
+        public string photoURL { get; set; }
+    }
+}
